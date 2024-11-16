11399458ae770281d042a8b37ead51c2c902e29d
Ahmad0301 <sehroz3040@gmail.com>
2024-11-16 13:52:27 +0500

    creat scheema

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ app.use(express.json())
 
 mongoose.connect('mongodb://localhost:27017').then(()=>console.log('mongodb is connected'))
 
+const BookSchema = new Schema({
+    title:{type:String, required:true},
+    author:{type:String, required:true},
+    year:Number,
+    genre:String,
+    summary:String
+})
+
 const port = 3000
 app.listen(port,()=>{
     console.log(`port listening on port ${ port }`)
